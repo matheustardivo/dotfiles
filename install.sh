367d70a1ec4ac3ab5ce1cbb8c39eb9acee7ad169
Matheus Tardivo <matheustardivo@gmail.com>
2012-04-19 14:04:11 -0300

    Install script

---
+++ install.sh
@@ -0,0 +1,9 @@
+cp files/bash_profile ~/.bash_profile
+cp files/git-completion.bash ~/.git-completion.bash
+cp files/gitconfig ~/.gitconfig
+cp files/gitignore_global ~/.gitignore_global
+cp files/irbrc ~/.irbrc
+cp files/pryrc ~/.pryrc
+
+source ~/.bash_profile
+open files/matheustardivo.itermcolors
