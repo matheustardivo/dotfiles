8f693824d96767f75050125d65cc15d03a8de7b4
Matheus Tardivo <matheustardivo@gmail.com>
2013-06-25 08:12:01 -0300

    Updated install.sh

--- install.sh
+++ install.sh
@@ -1,11 +1,14 @@
 cp files/bash_profile ~/.bash_profile
 cp files/gemrc ~/.gemrc
-cp files/git-completion.bash ~/.git-completion.bash
+cp files/git-completion.sh ~/.git-completion.sh
+cp files/git-prompt.sh ~/.git-prompt.sh
 cp files/gitconfig ~/.gitconfig
 cp files/gitignore_global ~/.gitignore_global
 cp files/irbrc ~/.irbrc
 cp files/pryrc ~/.pryrc
 cp files/zshrc ~/.zshrc
 
-# source ~/.bash_profile
 open files/idleToes.itermcolors
+open files/matheustardivo.itermcolors
+
+source ~/.bash_profile
