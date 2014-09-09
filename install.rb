fdfde80de7309399eefa74c25812e42dafd09675
Matheus Tardivo <matheustardivo@gmail.com>
2014-09-09 09:30:03 -0300

    Added new ruby install script - it's still really simple ;(

---
+++ install.rb
@@ -0,0 +1,24 @@
+#!/usr/bin/ruby
+
+require 'pathname'
+require 'fileutils'
+
+def dotfile_path(dotfile)
+  File.expand_path('~') + "/.#{dotfile.basename}"
+end
+
+def dotfile_backup(dotfile)
+  FileUtils.mv dotfile_path(dotfile), "#{dotfile_path(dotfile)}_backup_#{Time.now.to_f.to_s.gsub('.', '')}"
+end
+
+dotfiles = Pathname.new File.expand_path 'files'
+dotfiles.children.each do |dotfile|
+  # Verify if the dotfile already exist and if it's already a symlink before continue
+  next if File.exist?(dotfile_path(dotfile)) && File.symlink?(dotfile_path(dotfile))
+
+  # Try to move to *dotfile*_backup if dotfile exist but isn't a symlink
+  dotfile_backup(dotfile) if File.exist?(dotfile_path(dotfile))
+
+  # Symlink the dotfile to ~/.dotfile
+  File.symlink dotfile.to_s, File.expand_path('~') + "/.#{dotfile.basename}"
+end
